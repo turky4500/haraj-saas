708b2389dc2af32160aa370edfd66ef295dfae28
turky4500 <tur100@gmail.com>
2026-03-14 04:27:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.secret_key = "haraj_super_secret_key_v2"
-# إعداد قاعدة البيانات
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///haraj.db'
+
+# التعديل هنا: إجبار السيرفر على إنشاء قاعدة البيانات في نفس مجلد المشروع بالضبط
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'haraj.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
